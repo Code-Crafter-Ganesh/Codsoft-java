1deca3f7e88521fc4305ac228f6ae1ab3c99e978
Ganesh Rajkumar Shete <136620219+Code-Crafter-Ganesh@users.noreply.github.com>
2023-11-23 10:48:21 +0530

    Task 2  Student Grade Calculator code

---
+++ GradeCalculator.java
@@ -0,0 +1,59 @@
+import java.util.Scanner;
+
+public class GradeCalculator {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Student Grade Calculator");
+
+        // Input: Take marks obtained in each subject
+        System.out.print("Enter the number of subjects: ");
+        int numSubjects = scanner.nextInt();
+
+        int totalMarks = 0;
+
+        // Loop to take marks for each subject
+        for (int i = 1; i <= numSubjects; i++) {
+            System.out.print("Enter marks for subject " + i + " (out of 100): ");
+            int marks = scanner.nextInt();
+
+            // Validate marks
+            if (marks < 0 || marks > 100) {
+                System.out.println("Invalid marks. Please enter marks between 0 and 100.");
+                i--; // Decrement i to re-enter marks for the same subject
+            } else {
+                totalMarks += marks;
+            }
+        }
+
+        // Calculate Total Marks
+        System.out.println("\nTotal Marks: " + totalMarks);
+
+        // Calculate Average Percentage
+        double averagePercentage = (double) totalMarks / numSubjects;
+        System.out.println("Average Percentage: " + averagePercentage + "%");
+
+        // Grade Calculation
+        char grade = calculateGrade(averagePercentage);
+
+        // Display Results
+        System.out.println("Grade: " + grade);
+
+        scanner.close();
+    }
+
+    // Function to calculate grade based on average percentage
+    private static char calculateGrade(double averagePercentage) {
+        if (averagePercentage >= 90) {
+            return 'A';
+        } else if (averagePercentage >= 80) {
+            return 'B';
+        } else if (averagePercentage >= 70) {
+            return 'C';
+        } else if (averagePercentage >= 60) {
+            return 'D';
+        } else {
+            return 'F';
+        }
+    }
+}
